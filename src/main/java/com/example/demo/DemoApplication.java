8a8479885f817304fbb20ddf71e5a4d9b474c8a4
blooddot <blooddot@hotmail.com>
2022-09-29 11:53:04 +0800

    init

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,11 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RestController
+@SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-    @RequestMapping("/hello")
-    String index() {
-        return "hello spring boot";
-    }
-
 }
--- src/test/java/com/example/demo/UserControllerTests.java
+++ src/test/java/com/example/demo/UserControllerTests.java
@@ -9,10 +9,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
 import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -28,40 +28,11 @@ public class UserControllerTests {
     public void testUserController() throws Exception {
         RequestBuilder request;
 
-        request = get("/users/");
-        mvc.perform(request)
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("[]")));
-
-        request = post("/users/")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"id\":1,\"name\":\"tester\",\"age\":20}");
+        request = post("/create/")
+                .contentType(MediaType.APPLICATION_XML)
+                .content("<User><name>aaa</name><age>10</age></User>");
 
         mvc.perform(request)
-                .andExpect(content().string(equalTo("success")));
-
-        request = get("/users/");
-        mvc.perform(request)
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"tester\",\"age\":20}]")));
-
-        request = put("/users/1")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\":\"test master\",\"age\":30}");
-        mvc.perform(request)
-                .andExpect(content().string(equalTo("success")));
-
-        request = get("/users/1");
-        mvc.perform(request)
-                .andExpect(content().string(equalTo("{\"id\":1,\"name\":\"test master\",\"age\":30}")));
-
-        request = delete("/users/1");
-        mvc.perform(request)
-                .andExpect(content().string(equalTo("success")));
-
-        request = get("/users/");
-        mvc.perform(request)
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("[]")));
+                .andExpect(content().string(equalTo("<User><id/><name>xml tester: aaa</name><age>110</age></User>")));
     }
 }
