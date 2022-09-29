4ffe0b35d70a4c7ed67c838e35fc41fd22b5a008
blooddot <blooddot@hotmail.com>
2022-09-29 11:20:36 +0800

    init

--- src/main/sql/User.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `User` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `age` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
