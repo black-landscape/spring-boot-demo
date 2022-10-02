2ccaad8e81121789a07a4e7d4309a028001a43a2
blooddot <blooddot@hotmail.com>
2022-10-02 22:08:08 +0800

    init

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableCaching
 @SpringBootApplication
@@ -12,9 +14,31 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @RequestMapping("/hello")
-    String index() {
-        return "hello spring boot";
-    }
+    @RestController
+    static class HelloController {
 
+        @Autowired
+        private UserRepository userRepository;
+
+        @GetMapping("/create")
+        public void create() {
+            userRepository.save(new User("AAA", 10));
+        }
+
+        @GetMapping("/update")
+        public User update() {
+            User u1 = userRepository.findByName("AAA");
+            u1.setAge(20);
+            u1 = userRepository.save(u1);
+            return u1;
+        }
+
+        @GetMapping("/find")
+        public User find() {
+            User u1 = userRepository.findByName("AAA");
+            System.out.println("查询AAA用户：" + u1.getAge());
+            return u1;
+        }
+
+    }
 }
