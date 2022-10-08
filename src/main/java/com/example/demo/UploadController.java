1e4b346dd75febe2e453012a9367d7a8475a1529
blooddot <blooddot@hotmail.com>
2022-10-08 22:10:27 +0800

    init

---
+++ src/main/java/com/example/demo/UploadController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class UploadController {
+    @Value("${file.upload.path}")
+    private String path;
+
+    @GetMapping("/")
+    public String uploadPage() {
+        return "upload";
+    }
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public String create(@RequestPart MultipartFile file) throws Exception {
+        String fileName = file.getOriginalFilename();
+        String filePath = path + fileName;
+
+        File dest = new File(filePath);
+        Files.copy(file.getInputStream(), dest.toPath());
+        return "Upload file success : ";
+    }
+}
