00c9d5064ecedad31ea78daf641bd9aa1e2992dd
blooddot <blooddot@hotmail.com>
2022-10-10 13:00:23 +0800

    add: sharding job

--- src/main/java/com/example/demo/MySimpleJob.java
+++ src/main/java/com/example/demo/MySimpleJob.java
@@ -11,6 +11,16 @@ public class MySimpleJob implements SimpleJob {
 
     @Override
     public void execute(ShardingContext context) {
-        log.info("MySimpleJob start : blood.com {}", System.currentTimeMillis());
+        switch (context.getShardingItem()) {
+            case 0:
+                log.info("分片1：执行任务");
+                break;
+            case 1:
+                log.info("分片2：执行任务");
+                break;
+            case 2:
+                log.info("分片3：执行任务");
+                break;
+        }
     }
 }
